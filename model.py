f8121a411b94b88f1c7d68baebb8d25b1385a319
Olivia Haas <ohaas@wachtler9.cns.bzm>
2012-07-02 15:00:19 +0200

    plots pixel activations for all neurons and model stages. population code is plotted in the way that a negative neuronal activation equals a positive activation for the opposite (180 degree) neuron!

--- model.py
+++ model.py
@@ -21,9 +21,9 @@ def get_gauss_kernel(sigma, size, res):
     x,y = ny.mgrid[-size/2:size/2:res,-size/2:size/2:res]
     b=bivariate_normal(x,y,sigma,sigma)
     A=(1/ny.max(b))
+    B=A*bivariate_normal(x, y, sigma, sigma)
     #A=1
-    return x,y,A*bivariate_normal(x, y, sigma, sigma)
-
+    return x,y,B
 def gauss(sigma=0.75, x=ny.arange( 0.0, 8.0, 1), mu=0):
     return ny.exp(-(x-mu)**2/(2.0*sigma**2))
 
@@ -35,6 +35,8 @@ class Stage(object):
 
     def do_v1(self, net_in, net_fb):
         v1_t= net_in + (self.C * net_fb * net_in)
+
+        #v1_t= v1_t/ny.max(ny.absolute(v1_t))
         return v1_t
 
     def do_v2(self, v1_t):
@@ -48,19 +50,22 @@ class Stage(object):
         for n in ny.arange(0, v1_t.shape[2]):
             v2_t[:,:,n] = convolve2d (x[:,:,n], self.Gx, 'same')
 
-        return v1_t
+        v2_t= v2_t/ny.max(ny.absolute(v2_t))
+        return v2_t
 
     def do_v3(self, v2_t, j):
 
         s=v2_t.shape
         N=ny.zeros_like(v2_t)
         M=N
-        R=5
+        R=7  # CONSIDERED SURROUND IN EACH DIRECTION, PER PIXEL
 
+        # x,y IS THE AREA WHERE THE SURROUND R CAN BE CONSIDERED.
         a = ny.arange(R,s[0]-R)
         (x,y) = ny.meshgrid(a,a)
         (x,y) = (x.flatten(),y.flatten())
 
+        # PURPOSE OF t1-t11: CREATES AN ARRAY (x1,y1) WHICH FORMS THE FRAME (THICKNESS=R) OUTSIDE THE AREA (x,y) WHERE THE SURROUND CAN BE CONSIDERED
         t1=ny.arange(s[0])
         t2=ny.arange(R)
         t3,t4 = ny.meshgrid(t1,t2)
@@ -70,53 +75,39 @@ class Stage(object):
         t6,t7 = ny.meshgrid(t5,a)
         t6,t7 = t6.flatten(),t7.flatten()
 
-
         t8=ny.arange(s[0])
         t9=ny.arange(s[0]-R, s[0])
         t10,t11 = ny.meshgrid(t8,t9)
         t10,t11 = t10.flatten(),t11.flatten()
 
-
+        # ZEROS IN THE BEGINNING, SO THAT INDEX i IS AT THE CORRECT POSITION IN x1/y1 AFTER SURROUND-AVERAGE CALCULATION
         x1 = ny.concatenate((ny.tile(0.0,len(x)),t3,t6,t10))
         y1 = ny.concatenate((ny.tile(0.0,len(x)),t4,t7,t11))
+
         for i in ny.arange(len(x1)):
             if i in ny.arange(len(x)):
+
                 c1 = ny.arange(x[i]-R,x[i]+R+1)
                 c2 = ny.arange(y[i]-R,y[i]+R+1)
                 (m,n)=ny.meshgrid(c1,c2)
                 m=m.flatten()
                 n=n.flatten()
-                N[x[i],y[i],:]=ny.sum(v2_t[m,n,:], axis=0)
-                M[x[i],y[i],:]=N[x[i],y[i],:]/ny.max(N[x[i],y[i],:])
+                for k in ny.arange(0,8): # pixel-surround-average calculation per neuron (0-7)
+                    N[x[i],y[i],k]=ny.sum(v2_t[m,n,k], axis=0)
+                    M[x[i],y[i],k]=N[x[i],y[i],k]/((R+R+1)**2)
             else:
-                M[x1[i],y1[i],:]=v2_t[x1[i],y1[i],:]/ny.max(v2_t[x1[i],y1[i],:])
+                for k in ny.arange(0,8):
+                    M[x1[i],y1[i],k]=v2_t[x1[i],y1[i],k]/((R+R+1)**2)#ny.max(v2_t[x1[i],y1[i],k])
+        v3_t = v2_t+(v2_t-(0.5*M))#+4.0)/7.0)*(v2_t-(0.3*M)))#(v2_t-(0.3*M))#(((ny.exp(-j/2.3)+0.00)/1.00)*(v2_t-(0.3*M)))
 
-        z = v2_t+(((ny.exp(-j/2.3)+0.00)/1.00)*(v2_t-(0.3*M)))
-
-        v3_t=ny.divide(z,(ny.max(ny.absolute(z))))
-#
-#        j=11
-#        k=11
-#        print 'x=',j, 'and', 'y=', k
-#        print 'v2'
-#        print v2_t[j,k,:]
-#        print 'M'
-#        print M[j,k,:]
-#        print '--------------------------'
-#        v2_M= v2_t[j,k,:]-(0.3*M[j,k,:])
-#        print 'v2-M'
-#        print v2_M
-#        print '--------------------------'
-#        print '(v2+(v2-M))/max'
-#        print (v2_t[j,k,:]+v2_M)/ny.max(ny.absolute(v2_t[j,k,:]+v2_M))
-#        print '---------------------------'
+        v3_t = v3_t/ny.max(ny.absolute(v3_t))
         return v3_t
 
     def do_all(self, net_in, net_fb, i):
         v1_t = self.do_v1(net_in, net_fb)
         v2_t = self.do_v2(v1_t)
         v3_t = self.do_v3(v2_t, i)
-        return v3_t
+        return v1_t,v2_t,v3_t
 
 
 class Model(object):
@@ -134,12 +125,13 @@ class Model(object):
         self.time_frames = cfg.getint('Input', 'time_frames')
         self.gauss_width = cfg.getfloat('PopCode', 'neuron_sigma')
 
-        v1_C = cfg.getint('V1', 'C')
+        v1_C = cfg.getfloat('V1', 'C')
         self.mt_kernel_sigma =  cfg.getfloat('MT', 'kernel_sigma')
         self.mt_kernel_size =  cfg.getfloat('MT', 'kernel_size')
         self.mt_kernel_res =  cfg.getfloat('MT', 'kernel_res')
 
         self.x,self.y,self.mt_gauss = get_gauss_kernel(self.mt_kernel_sigma, self.mt_kernel_size, self.mt_kernel_res)
+
 
         self.V1 = Stage([], v1_C)
         self.MT = Stage(self.mt_gauss, 0)
@@ -149,33 +141,44 @@ class Model(object):
         """
         definition of initial population codes for different time steps (is always the same one!!!)
         """
-        I = ny.zeros((self.main_size, self.main_size, 8, self.time_frames+1))
-        for i in ny.arange(0, self.time_frames+1):
-            j = pc.Population(self.main_size, self.square_size, self.gauss_width)
-            I[:,:,:,i] = j.initial_pop_code()
-            #j.show_vectors(I[:,:,:,i])
+        j = pc.Population(self.main_size, self.square_size, self.gauss_width)
+        I= j.initial_pop_code()
 
         return I
 
 
     def run_model_full(self, i):
         self.input = self.create_input()
-
+        pop = pc.Population(self.main_size, self.square_size, self.gauss_width)
         X = ny.zeros((self.main_size, self.main_size, 8, self.time_frames+1))
         FB=ny.zeros((self.main_size, self.main_size, 8, self.time_frames+1))
-        X[:,:,:,0] = self.input[:,:,:,0]
+        c=0
+        if i==0:
+            X[:,:,:,i] = self.input
+            if self.time_frames==0 and c==1:
+                pp.figure(5)
+                pop.twoD_activation(X[:,:,:,i], 0, i)
 
+        else:
+            inp = self.input
 
-        for d_t in ny.arange(1, self.time_frames+1):
+            v1_a, v1_b, v1_c = self.V1.do_all(inp, FB[:,:,:,i-1],i)
+            if c==1:
+                pp.figure(5+i-1)
+                pop.twoD_activation(v1_a, 0, i)
+                pop.twoD_activation(v1_b, 1, i)
+                pop.twoD_activation(v1_c, 2, i)
 
-            inp = self.input[:,:,:,d_t-1]
-            v1 = self.V1.do_all(inp, FB[:,:,:,d_t-1],i)
-            mt = self.MT.do_all(v1, 0, i)
-            X[:,:,:, d_t] = mt
-
+            mt_a, mt_b, mt_c = self.MT.do_all(v1_c, 0, i)
+            if c==1:
+                pp.figure(5+i-1)
+                pop.twoD_activation(mt_a, 3, i)
+                pop.twoD_activation(mt_b, 4, i)
+                pop.twoD_activation(mt_c, 5, i)
+            X[:,:,:, i] = mt_c
 
             if self.do_feedback:
-                FB[:,:,:,d_t] = X[:,:,:,d_t]
+                FB[:,:,:,i] = X[:,:,:,i]
 
 
         return X,FB
@@ -184,33 +187,55 @@ class Model(object):
     def integrated_motion_direction(self):
 
 
+        A = ny.zeros((self.main_size,self.main_size,self.time_frames+1))
 
         for i in ny.arange(0,self.time_frames+1):
-            X,FB = self.run_model_full (i)
+
             pop = pc.Population(self.main_size, self.square_size, self.gauss_width)
+            X,FB = self.run_model_full (i)
 
             if i>0:
 
-                pp.figure(2)
-                pop.show_vectors(X[:,:,:,i],self.time_frames,i)
-
-                pp.figure(3+self.time_frames)
-                pop.plot_row(1,15,X[:,:,:,i],i, self.time_frames)
+#                pp.figure(2)
+#                pop.show_vectors(X[:,:,:,i],self.time_frames,i)
 
                 pp.figure(1)
                 pop.plot_pop(X[:,:,:,i],self.time_frames,i)
+
+                pp.figure(2)
+                pop.plot_cartesian_pop(X[:,:,:,i],self.time_frames,i)
+
+#                pp.figure(3)
+#                pop.heat_map(X[:,:,:,i], i, self.time_frames, A)
+
+                pp.figure(4)
+                pop.plot_row(1,15,X[:,:,:,i],i, self.time_frames)
+
+#                pp.figure(5)
+#                pop.twoD_activation(X[:,:,:,i], i, self.time_frames)
 
             else:
                 pp.figure(1)
                 pop.plot_pop(X[:,:,:,i],self.time_frames,i)
-
-                pp.figure(3+self.time_frames)
-                pop.plot_row(1,15,X[:,:,:,i],i, self.time_frames)
+                #pp.suptitle('For $v^{3}=v^{2}+\\frac{1}{2}$ $5$ $(\mu=5\degree)$')
+                pp.suptitle('$For$ $v^{(3)}=v^{(2)}+\\frac{e^{(\\frac{-x}{2.3})}\cdot(v^{(2)}-(0.3\cdot M))}{max}$', fontsize=30)
 
                 pp.figure(2)
-                pop.show_vectors(self.input[:,:,:,i], self.time_frames,i)
-                pp.suptitle('For spatial kernel values:%s=%.2f, size=%.2f, \n res=%.2f and neuron kernel %s=%.2f'\
-                % (u"\u03C3",self.mt_kernel_sigma, self.mt_kernel_size, self.mt_kernel_res,u"\u03C3", self.gauss_width) )
+                pop.plot_cartesian_pop(X[:,:,:,i],self.time_frames,i)
+
+#                pp.figure(3)
+#                pop.heat_map(X[:,:,:,i], i, self.time_frames, A)
+
+                pp.figure(4)
+                pop.plot_row(1,15,X[:,:,:,i],i, self.time_frames)
+
+#                pp.figure(5)
+#                pop.twoD_activation(X[:,:,:,i], i, self.time_frames)
+
+#                pp.figure(2)
+#                pop.show_vectors(self.input[:,:,:,i], self.time_frames,i)
+#                pp.suptitle('For spatial kernel values:%s=%.2f, size=%.2f, res=%.2f and neuron kernel %s=%.2f.\n The y axis indicates the direction in color space.'\
+#                % (u"\u03C3",self.mt_kernel_sigma, self.mt_kernel_size, self.mt_kernel_res,u"\u03C3", self.gauss_width) )
 
             print i
 
